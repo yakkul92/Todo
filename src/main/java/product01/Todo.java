d5f0edfd863c84c0b73754681e9df5c11e056558
yakkul92 <sasapanda0902@gmail.com>
2024-02-08 23:00:00 +0900

    [create]todo class

--- src/main/java/product01/Todo.java
+++ src/main/java/product01/Todo.java
@@ -5,6 +5,28 @@ import java.io.*;
 
 public class Todo{
     public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ToDo> list = new ArrayList<>();
+        while(true){
+          System.out.println("??操作を入力してください。??");
+          System.out.print("1/登録 2/重要度変更 3/削除 4/終了>");
+          int select = sc.nextInt();
+          switch(select){
+            case 1:
+              addItem(list,sc);
+              break;
+            case 2:
+              updateItem(list,sc);
+              break;
+            case 3:
+              deleteItem(list,sc);
+              break;
+            default:
+              System.out.println("アプリケーションを終了します。");
+              return;
+          }
+          displayList(list);
+        }
     }
   }
   class ToDo{
